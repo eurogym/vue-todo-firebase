75103beb18ed6f09a14d4085194a33032e527e9c
Gerald Landl <glandl@eurogym.info>
2022-04-17 11:49:12 +0200

    Update TodoForm.vue

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form @submit.prevent="addTodo">
+    <MDBInput type="text" v-model="newTodo" placeholder="Add a todo" />
+    <MDBBtn color="primary">Add</MDBBtn>
+  </form>
+</template>
+
+<script>
+import { MDBInput, MDBBtn } from 'mdb-vue-ui-kit';
+
+export default {
+  components: {
+    MDBInput,
+    MDBBtn
+  },
+}
+</script>
+
+<style>
+
+</style>
