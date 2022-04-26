bdbfd67e040d52e13baa7e71f80cbbaa6035d7bd
glandl <glandl@eurogym.info>
2022-04-26 21:23:26 +0200

    form data binding

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,22 +1,31 @@
 <template>
   <form action="">
     <label for="">Title</label>
-    <input type="text"><br>
+    <input type="text" v-model="todoitem.title"><br>
     <label for="">Description</label>
-    <textarea id="" cols="30" rows="10"></textarea><br>
+    <textarea id="" cols="30" rows="10" v-model="todoitem.description"></textarea><br>
     <label for="">Due Date</label>
-    <input type="date"><br>
+    <input type="date" v-model="todoitem.duedate"><br>
     <button>add</button>
     <button>cancel</button>
   </form>
 </template>
 
 <script>
-
 export default {
-};
+  name: "TodoForm",
+  data() {
+    return {
+      todoitem : {
+        title : "",
+        description: "",
+        duedate: new Date().toISOString().slice(0, 10),
+        done: false
+      },
+    }
+  }
+}
 </script>
 
 <style>
 </style>
-
