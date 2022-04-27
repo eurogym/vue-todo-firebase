1f3543143bfb303d44c591baf551b25a8823cf49
Gerald Landl <glandl@duality.at>
2022-04-27 22:47:13 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "???",
+  apiKey: "AIzaSyBO6GwVoWpib1ER5lXVIR18Py5MEy7dbrU",
   authDomain: "vue-todo-list-2fd80.firebaseapp.com",
   databaseURL: "https://vue-todo-list-2fd80-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "vue-todo-list-2fd80",
   storageBucket: "vue-todo-list-2fd80.appspot.com",
-  messagingSenderId: "???",
-  appId: "???"
+  messagingSenderId: "1093609942299",
+  appId: "1:1093609942299:web:f2050bdf30a4f20ac877be"
 }
 
 // Initialize Firebase
